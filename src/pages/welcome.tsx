d4bcbcd53a4edbf49a91e144c184c2025eeffa83
Diego Melendez <diedu89@gmail.com>
2018-10-06 01:55:33 -0600

    fix welcome page

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -21,8 +21,10 @@ class Welcome extends Page<WelcomeProps> {
               </div>
             </div>
             <div className="row justify-content-center">
-              <div className="col-6 concept-text">
-                Surge como mi propio metodo de comunicacion a traves de reiterados procesos
+              <div className="col-6 concept-text" style={{padding:0, marginTop:'3%'}}>
+                Surge como mi propio metodo de comunicacion a traves de reiterados procesos <br />
+                ilustrativos, donde el trazo es mi principal portavoz, adaptable a distintas <br />
+                situaciones que requieran soluciones graficas/editoriales.
               </div>
             </div>
           </div>
